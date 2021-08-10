645c22cbde4d70bed467df0c102b1de7dc73f0fc
@shubham-root <shubham.root@gmail.com>
2021-08-10 17:28:53 +0530

    Updated for newer module-sdk and version bumped

--- src/nodes/listMeetingRegistrants/listMeetingRegistrants.schema.js
+++ src/nodes/listMeetingRegistrants/listMeetingRegistrants.schema.js
@@ -1,63 +1,98 @@
-const {
-    Node,
-    Schema,
-    fields
-} = require('@mayahq/module-sdk')
+const { Node, Schema, fields } = require("@mayahq/module-sdk");
 const ZoomAuth = require("../zoomAuth/zoomAuth.schema");
 
 class ListMeetingRegistrants extends Node {
-    constructor(node, RED) {
-        super(node, RED)
+	constructor(node, RED, opts) {
+        super(node, RED, {
+            ...opts
+        })
     }
 
-    static schema = new Schema({
-        name: 'list-meeting-registrants',
-        label: 'list-meeting-registrants',
-        category: 'Maya Red Zoom',
-        isConfig: false,
-        fields: {
-            session: new fields.ConfigNode({type: ZoomAuth}),
-            // convert long to string before storing
-            meetingId: new fields.Typed({type: 'str', defaultVal: '', allowedTypes: ['msg', 'flow', 'global']}),
-            pageSize: new fields.Typed({type: 'num', defaultVal: 10, allowedTypes: ['msg', 'flow', 'global']}),
-            //nextPageToken: new fields.Typed({type: 'str', defaultVal: '', allowedTypes: ['msg', 'flow', 'global']}),
-        },
+	static schema = new Schema({
+		name: "list-meeting-registrants",
+		label: "list-meeting-registrants",
+		category: "Maya Red Zoom",
+		icon: "zoom.png",
+		isConfig: false,
+		fields: {
+			// convert long to string before storing
+			meetingId: new fields.Typed({
+				type: "str",
+				defaultVal: "",
+				allowedTypes: ["msg", "flow", "global"],
+			}),
+			pageSize: new fields.Typed({
+				type: "num",
+				defaultVal: 10,
+				allowedTypes: ["msg", "flow", "global"],
+			}),
+			//nextPageToken: new fields.Typed({type: 'str', defaultVal: '', allowedTypes: ['msg', 'flow', 'global']}),
+		},
+	});
 
-    })
+	onInit() {
+		// Do something on initialization of node
+	}
 
-    onInit() {
-        // Do something on initialization of node
-    }
-
-    async onMessage(msg, vals) {
-        this.setStatus("PROGRESS", "fetching zoom meeting registrants...");
-        var fetch = require("node-fetch"); // or fetch() is native in browsers
-        try{
-            let res = await fetch(`https://api.zoom.us/v2/meetings/${vals.meetingId}/registrants?page_size=${vals.pageSize}`, 
-            {
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${this.credentials.session.access_token}`,
-                    "Content-Type":"application/json"
-                }
-            });
-            let json = await res.json();
-            if(json.error){
-                msg.error = json.error;
-                this.setStatus("ERROR", json.error.message);
+	async onMessage(msg, vals) {
+		this.setStatus("PROGRESS", "fetching zoom meeting registrants...");
+		var fetch = require("node-fetch"); // or fetch() is native in browsers
+		try {
+			const fetchConfig = {
+				url: `https://api.zoom.us/v2/meetings/${vals.meetingId}/registrants?page_size=${vals.pageSize}`,
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${this.tokens.vals.access_token}`,
+					"Content-Type": "application/json",
+				},
+			};
+			let res = await fetch(fetchConfig.url, {
+				method: fetchConfig.method,
+				headers: fetchConfig.headers,
+			});
+			let json = await res.json();
+			if (json.error) {
+				if (json.error.code === 401) {
+					const { access_token } = await this.refreshTokens();
+					if (!access_token) {
+						this.setStatus("ERROR", "Failed to refresh access token");
+						msg.isError = true;
+						msg.error = {
+							reason: "TOKEN_REFRESH_FAILED",
+						};
+						return msg;
+					}
+					fetchConfig.headers.Authorization = `Bearer ${access_token}`;
+					res = await fetch(fetchConfig.url, {
+						method: fetchConfig.method,
+						headers: fetchConfig.headers,
+					});
+					json = await res.json();
+					if (json.error) {
+						msg.error = json.error;
+						this.setStatus("ERROR", json.error.message);
+						return msg;
+					} else {
+						msg.payload = json;
+						this.setStatus("SUCCESS", "Fetched Meeting registrants");
+						return msg;
+					}
+				} else {
+					msg.error = json.error;
+					this.setStatus("ERROR", json.error.message);
+					return msg;
+				}
+			} else {
+                msg.payload = json;
+                this.setStatus("SUCCESS", "Fetched Meeting registrants");
                 return msg;
             }
-            msg.payload = json;
-            this.setStatus("SUCCESS", "fetched");
-            return msg;
-        }
-        catch(err){
-            msg.error = err;
-            this.setStatus("ERROR", "error occurred");
-            return msg;
-        }
-
-    }
+		} catch (err) {
+			msg.error = err;
+			this.setStatus("ERROR", "error occurred");
+			return msg;
+		}
+	}
 }
 
-module.exports = ListMeetingRegistrants
+module.exports = ListMeetingRegistrants;
--- src/nodes/listMeetings/listMeetings.schema.js
+++ src/nodes/listMeetings/listMeetings.schema.js
@@ -6,17 +6,19 @@ const {
 const ZoomAuth = require("../zoomAuth/zoomAuth.schema");
 
 class ListMeetings extends Node {
-    constructor(node, RED) {
-        super(node, RED)
+    constructor(node, RED, opts) {
+        super(node, RED, {
+            ...opts
+        })
     }
 
     static schema = new Schema({
         name: 'list-meetings',
         label: 'list-meetings',
         category: 'Maya Red Zoom',
+        icon: 'zoom.png',
         isConfig: false,
         fields: {
-            session: new fields.ConfigNode({type: ZoomAuth}),
             userId: new fields.Typed({type: 'str', defaultVal: 'me', allowedTypes: ['msg', 'flow', 'global']}),
             meetingType: new fields.Select({ options: ['upcoming','scheduled', 'live'], defaultVal: 'upcoming' }),
             pageSize: new fields.Typed({type: 'num', defaultVal: 10, allowedTypes: ['msg', 'flow', 'global']}),
@@ -32,30 +34,61 @@ class ListMeetings extends Node {
     async onMessage(msg, vals) {
         this.setStatus("PROGRESS", "fetching zoom meetings...");
         var fetch = require("node-fetch"); // or fetch() is native in browsers
-        try{
-            let res = await fetch(`https://api.zoom.us/v2/users/${vals.userId}/meetings?type=${vals.meetingType}&page_size=${vals.pageSize}`, 
-            {
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${this.credentials.session.access_token}`,
-                    "Content-Type":"application/json"
-                }
-            });
-            let json = await res.json();
-            if(json.error){
-                msg.error = json.error;
-                this.setStatus("ERROR", json.error.message);
+        try {
+			const fetchConfig = {
+				url: `https://api.zoom.us/v2/users/${vals.userId}/meetings?type=${vals.meetingType}&page_size=${vals.pageSize}`,
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${this.tokens.vals.access_token}`,
+					"Content-Type": "application/json",
+				},
+			};
+			let res = await fetch(fetchConfig.url, {
+				method: fetchConfig.method,
+				headers: fetchConfig.headers,
+			});
+			let json = await res.json();
+			if (json.error) {
+				if (json.error.code === 401) {
+					const { access_token } = await this.refreshTokens();
+					if (!access_token) {
+						this.setStatus("ERROR", "Failed to refresh access token");
+						msg.isError = true;
+						msg.error = {
+							reason: "TOKEN_REFRESH_FAILED",
+						};
+						return msg;
+					}
+					fetchConfig.headers.Authorization = `Bearer ${access_token}`;
+					res = await fetch(fetchConfig.url, {
+						method: fetchConfig.method,
+						headers: fetchConfig.headers,
+					});
+					json = await res.json();
+					if (json.error) {
+						msg.error = json.error;
+						this.setStatus("ERROR", json.error.message);
+						return msg;
+					} else {
+						msg.payload = json;
+						this.setStatus("SUCCESS", "Fetched Meetings");
+						return msg;
+					}
+				} else {
+					msg.error = json.error;
+					this.setStatus("ERROR", json.error.message);
+					return msg;
+				}
+			} else {
+                msg.payload = json;
+                this.setStatus("SUCCESS", "Fetched Meetings");
                 return msg;
             }
-            msg.payload = json;
-            this.setStatus("SUCCESS", "fetched");
-            return msg;
-        }
-        catch(err){
-            msg.error = err;
-            this.setStatus("ERROR", "error occurred");
-            return msg;
-        }
+		} catch (err) {
+			msg.error = err;
+			this.setStatus("ERROR", "error occurred");
+			return msg;
+		}
     }
 }
 
